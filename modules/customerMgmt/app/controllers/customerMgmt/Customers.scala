3966d0766e9a20e47fcdc4ff9166bc4906e24616
Nick Vintila <nick@semanticbeeng.com>
2014-12-06 14:02:46 -0800

    wip: providing a JSON data api

--- modules/customerMgmt/app/controllers/customerMgmt/Customers.scala
+++ modules/customerMgmt/app/controllers/customerMgmt/Customers.scala
@@ -2,106 +2,90 @@ package controllers.customerMgmt
 
 import controllers.common.RenderMultipleFormats
 import model.customerMgmt.Customer
-import play.api.libs.functional.syntax._
 import play.api.libs.json._
 import play.api.mvc._
 
 object Customers extends RenderMultipleFormats[Customer] {
 
-  def someCustomers : List[Customer] = {
-
-    List(Customer("Joe", "Smith"), Customer("Oliver", "Twist"))
-  }
-
-
   //  val form = Form(
-//    mapping(
-//      "id" -> ignored(NotAssigned: Pk[Int]),
-//      "username" -> nonEmptyText,
-//      "age" -> number)(User.apply)(User.unapply))
-//
-  def list = Action { implicit request =>
-    render {
-      //case Accepts.Html() => Ok(views.html.users.list(User.list))
-      case Accepts.Json() => Ok(Json.toJson(someCustomers))
-    }
+  //    mapping(
+  //      "id" -> ignored(NotAssigned: Pk[Int]),
+  //      "username" -> nonEmptyText,
+  //      "age" -> number)(User.apply)(User.unapply))
+  //
+
+  def list = Action {
+    Ok(Json.toJson(Customer.someCustomers))
   }
 
-//  implicit val customerrites = new Writes[Customer] {
-//    def writes(customer: Customer) = {
-//      Json.obj(
-//        "firstName" -> JsString(customer.firstName),
-//        "lastName" -> JsString(customer.lastName))
-//    }
-//  }
-//
-//  val extendedCustomerWrites = new Writes[Customer] {
-//    def writes(customer: Customer) = {
-//      customerrites.writes(customer) /*+ ("contacts" -> Writes.seq(contactWrites).writes(user.contacts))*/
-//    }
-//  }
+  //  def list = Action { implicit request =>
+  //    render {
+  //      //case Accepts.Html() => Ok(views.html.users.list(User.list))
+  //      case Accepts.Json() => Ok(Json.toJson(someCustomers))
+  //    }
+  //  }
 
-  implicit val customerWrite = {
-    import play.api.libs.json.Writes.path._
-    (
-      at[String](__ \ "firstName") and
-        at[String](__ \ "lastName")
-      )(unlift(Customer.unapply))
-  }
+  //  val extendedCustomerWrites = new Writes[Customer] {
+  //    def writes(customer: Customer) = {
+  //      customerrites.writes(customer) /*+ ("contacts" -> Writes.seq(contactWrites).writes(user.contacts))*/
+  //    }
+  //  }
 
 
   //  val contactWrites = new Writes[Contact] {
-//    def writes(contact: Contact) = {
-//      Json.obj(
-//        "id" -> JsNumber(contact.id.get),
-//        "contactType" -> JsString(contact.contactType),
-//        "contact" -> JsString(contact.contact))
-//    }
-//  }
-  
+  //    def writes(contact: Contact) = {
+  //      Json.obj(
+  //        "id" -> JsNumber(contact.id.get),
+  //        "contactType" -> JsString(contact.contactType),
+  //        "contact" -> JsString(contact.contact))
+  //    }
+  //  }
+
   def find(name: String) = Action { implicit request =>
-//    User.load(id).map { user =>
-//    render {
-//      case Accepts.Html() => Ok(views.html.users.show(user))
-//      case Accepts.Json() => Ok(Json.toJson(user)(extendedUserWrites))
-//    }
-//    }.getOrElse(NotFound)
+    //    User.load(id).map { user =>
+    //    render {
+    //      case Accepts.Html() => Ok(views.html.users.show(user))
+    //      case Accepts.Json() => Ok(Json.toJson(user)(extendedUserWrites))
+    //    }
+    //    }.getOrElse(NotFound)
     NotFound
   }
-  
-//  def add = Action {
-//    Ok(views.html.users.add(form))
-//  }
 
-//  def save = Action { implicit request =>
-//    form.bindFromRequest.fold(
-//      formWithErrors => handleError(formWithErrors, views.html.users.add(formWithErrors)),
-//      user => {
-//        User.save(user)
-//        handleSuccess(routes.Users.list, "User successfully created!")
-//      })
-//  }
-//
-//  def edit(id: Int) = Action {
-//    User.load(id).map { user =>
-//      val bindedForm = form.fill(user)
-//      Ok(views.html.users.edit(id, bindedForm))
-//    }.getOrElse(NotFound)
-//  }
-//
-//  def update(id: Int) = Action { implicit request =>
-//    User.load(id).map { user =>
-//      form.bindFromRequest.fold(
-//      formWithErrors => handleError(formWithErrors,views.html.users.edit(id, formWithErrors)),
-//        userWithNewValues => {
-//          User.update(id, userWithNewValues)
-//          handleSuccess(routes.Users.show(id), "User successfully updated!")
-//        })
-//    }.getOrElse(NotFound)
-//  }
-//
-//  def delete(id: Int) = Action { implicit request =>
-//    User.delete(id)
-//    handleSuccess(routes.Users.list, "User successfully deleted!")
-//  }
+  //  def add = Action {
+  //    Ok(views.html.users.add(form))
+  //  }
+
+  //  def save = Action { implicit request =>
+  //    form.bindFromRequest.fold(
+  //      formWithErrors => handleError(formWithErrors, views.html.users.add(formWithErrors)),
+  //      user => {
+  //        User.save(user)
+  //        handleSuccess(routes.Users.list, "User successfully created!")
+  //      })
+  //  }
+  //
+  //  def edit(id: Int) = Action {
+  //    User.load(id).map { user =>
+  //      val bindedForm = form.fill(user)
+  //      Ok(views.html.users.edit(id, bindedForm))
+  //    }.getOrElse(NotFound)
+  //  }
+  //
+  //  def update(id: Int) = Action { implicit request =>
+  //    User.load(id).map { user =>
+  //      form.bindFromRequest.fold(
+  //      formWithErrors => handleError(formWithErrors,views.html.users.edit(id, formWithErrors)),
+  //        userWithNewValues => {
+  //          User.update(id, userWithNewValues)
+  //          handleSuccess(routes.Users.show(id), "User successfully updated!")
+  //        })
+  //    }.getOrElse(NotFound)
+  //  }
+  //
+  //  def delete(id: Int) = Action { implicit request =>
+  //    User.delete(id)
+  //    handleSuccess(routes.Users.list, "User successfully deleted!")
+  //  }
+
+
 }
--- modules/customerMgmt/app/model/customerMgmt/Customer.scala
+++ modules/customerMgmt/app/model/customerMgmt/Customer.scala
@@ -1,7 +1,57 @@
 package model.customerMgmt
 
+import play.api.libs.json._
+
 /**
  *
  */
-case class Customer (firstName : String, lastName : String) {
+case class Customer(firstName: String, lastName: String, contacts: List[EmailAddress]) {
 }
+
+//case class Contact()
+
+case class EmailAddress(address: String) /*extends Contact*/ {
+}
+
+object EmailAddress {
+  implicit val emailAddressFormat = Json.format[EmailAddress]
+}
+
+case class PhoneNumber(prefix: String, number: String, extension: String) /*extends Contact*/ {
+}
+
+object PhoneNumber {
+  implicit val phoneNumberFormat = Json.format[PhoneNumber]
+}
+
+object Customer {
+
+  implicit val customerFormat = Json.format[Customer]
+//  implicit val customersFormat = Json.format[List[Customer]]
+
+  def someCustomers: List[Customer] = {
+
+    val contacts = List(EmailAddress("joe@smith.com"))
+    List(Customer("Joe", "Smith", List()), Customer("Oliver", "Twist", contacts))
+  }
+
+}
+
+
+
+//object Formats {
+//  implicit val customersFormat = Json.format[List[Customer]]
+//  implicit val customerFormat = Json.format[Customer]
+//  implicit val emailAddressFormat = Json.format[EmailAddress]
+//  implicit val phoneNumberFormat = Json.format[PhoneNumber]
+//
+//}
+
+//implicit val customerWrite = {
+//import play.api.libs.json.Writes.path._
+//(
+//at[String](__ \ "firstName") and
+//at[String](__ \ "lastName") and
+//at[Option[List[model.customerMgmt.Contact]]](__ \ "contacts")
+//)(unlift(Customer.unapply))
+//}
